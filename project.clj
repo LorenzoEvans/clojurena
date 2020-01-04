15ad506ec3d8fac8c1af855b6147b3beddea4aaf
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-01-03 22:48:59 -0500

    env vars set up

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [clj-http "3.10.0"]
-                 [org.clojure/data.json "0.2.6"]]
+                 [org.clojure/data.json "0.2.6"]
+                 [environ "1.1.0"]]
   :main ^:skip-aot clojurena.core
   :profiles {:uberjar {:aot :all}})
--- src/clojurena/API/Authentication/auth.clj
+++ src/clojurena/API/Authentication/auth.clj
@@ -1,2 +1,7 @@
 (ns clojurena.API.Authentication.auth
-    (:require [clj-http :as client]))
+    (:require [clj-http :as client]
+              [environ.core :refer [env]]))
+
+(def app-id (env :app-id))
+(def app-secret (env :app-sec-key))
+(def cb-url (env :cb-url))
