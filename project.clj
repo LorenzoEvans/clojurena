7f1e3c80c57e59fdc7eda85ca87bcbcde1f0e8f3
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-01-03 20:22:51 -0500

    working out these api calls

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
                  [http-kit "2.3.0"]
+                 [clj-http "3.10.0"]
                  [ring/ring-defaults "0.3.2"]
                  [org.clojure/data.json "0.2.6"]]
   :main ^:skip-aot clojurena.core
--- src/clojurena/core.clj
+++ src/clojurena/core.clj
@@ -1,7 +1,14 @@
 (ns clojurena.core
-  (:require [compojure.core :refer :all]))
+  (:require [compojure.core :refer :all]
+            [compojure.response :as res]
+            [clojure.data.json :as json]))
 
 (defn -main
   "I don't do a whole lot."
   []
   (println "Hello, World!"))
+
+(def block 
+  (GET "https://api.are.na/v2/blocks/:1" []))
+
+(println (compojure.response/render block :get))
