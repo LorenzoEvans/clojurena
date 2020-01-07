35185d4ec2d5525f37fbc165f329a8b3e71e37f8
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-01-07 18:11:24 -0500

    this is pretty cool

--- src/clojurena/API/Channels/channel_post_requests.clj
+++ src/clojurena/API/Channels/channel_post_requests.clj
@@ -2,6 +2,7 @@
  (:require [clj-http.client :as client]
            [clojure.string :as str]
            [clojurena.API.Channels.channel-get-requests :refer [base-url]]
+           [clojurena.API.Blocks.block-post-requests :refer [post-block]]
            [clojurena.API.utils :refer [async]]))
         
 
@@ -20,4 +21,7 @@
     (client/post (str base-url channel-name "/" "collaborators") {:body ids}
       (fn [response] (println "Response is: " response) response)
       (fn [exception] (println "Exception is: " exception) exception)))) 
+  
+(defn post-block-to-channel [channel-name source auth & content user pass]
+  (post-block channel-name source auth & content user pass))
   
--- src/clojurena/API/Channels/channel_put_requests.clj
+++ src/clojurena/API/Channels/channel_put_requests.clj
@@ -1,2 +1,9 @@
 (ns clojurena.API.Channels.channel-put-requests
  (:require [clj-http.client :as client]))
+
+(defn update-channel [])
+
+(defn update-order [])
+
+
+
