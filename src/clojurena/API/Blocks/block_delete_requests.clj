4ca2e729c38158c7a1e50f0e33ad0b480894ad83
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-01-27 17:31:22 -0500

    alright, I see what is ahead of me..

--- src/clojurena/API/Blocks/block_delete_requests.clj
+++ src/clojurena/API/Blocks/block_delete_requests.clj
@@ -6,10 +6,12 @@
 
 (def default-url "https://api.are.na/v2/channel/")
 
-(defn delete-block [slug block-id & auth-token]
+(defn delete-block [slug channel-id block-id & auth-token]
     "Deletes/removes connection between a block and channel, requires authentication"
+  ; so we have to get the channel, pull the channel-id off of it, then pipe that bit of info
+  ; into the delete request....bet!
     (async
-        (client/delete (str default-url slug "/" "blocks" "/" block-id)
+        (client/delete (str default-url slug "/" channel-id "/" "blocks" "/" block-id)
                        {:async? true
                         :headers {"Authorization" auth-token}}
          (fn [response] (println "Response is: " response) response)
