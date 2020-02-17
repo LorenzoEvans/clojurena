2dd02ab92ad2f348d584167278dd425a59430987
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-02-17 17:28:40 -0500

    Time to clean up files

--- test/core_test.clj
+++ test/core_test.clj
@@ -15,9 +15,6 @@
             [clojurena.API]
             [clojurena.API]))
             
-            
-            
-            
 ; Block delete tests
 (deftest delete-block
   (testing "Ability to delete blocks from specified channels"
@@ -38,12 +35,20 @@
   (get-blocks-channels-test))
 
 ; Block put tests TBD
+; put-block
+; toggle-block-status
+
+; Block posts tests TBD
+; post-block
 
 ; Take advantage of fixtures for destructive tests.
+; How are we going to allow users to properly authenticate themselves
+; for editing their own accounts?
+; We need to figure that out before we can test these operations.
 
-(deftest update-block-test
-  (testing "Ability to update specific block in channel."
-    (is (= (block-put/put-block)))))
+; (deftest update-block-test
+;   (testing "Ability to update specific block in channel."
+;     (is (= (block-put/put-block)))))
 
 ; Test toggle block
   
@@ -51,3 +56,112 @@
 ; (deftest post-block-test
 ;   (testing "Ability to post a block to a specific channel."
 ;     (is (= (post )))))
+
+
+; Channel delete tests TBD
+; delete-channel
+; delete-collaborators
+; Take advantage of fixtures for destructive tests.
+
+; Channel get tests
+(deftest get-all-channels-test
+  (testing "Ability to retrieve all public are.na channels."
+    (is (= (:status (channel-get/all-public-channels)) 200))))
+
+(deftest get-single-channel-test
+  (testing "Ability to retrieve single channel."
+    (is (= (:status (channel-get/get-single-channel "geosymgif")) 200))))
+
+(deftest get-channel-thumb-test
+  (testing "Ability to retrieve single channel."
+    (is (= (:status (channel-get/get-channel-thumb "geosymgif")) 200))))
+
+(deftest get-channel-connections-test
+  (testing "Ability to retrieve single channel."
+    (is (= (:status (channel-get/get-channel-connections "geosymgif")) 200))))
+
+(deftest get-connected-channels-test
+  (testing "Ability to retrieve single channel."
+    (is (= (:status (channel-get/get-connected-channels "geosymgif")) 200))))
+
+(deftest get-channel-contents-test
+  (testing "Ability to retrieve single channel."
+    (is (= (:status (channel-get/get-channel-contents "geosymgif")) 200))))
+
+(deftest channel-get-tests
+  (get-all-channels-test)
+  (get-single-channel-test)
+  (get-channel-thumb-test)
+  (get-channel-connections-test)
+  (get-connected-channels-test)
+  (get-channel-contents-test))
+
+
+; Channel Post tests TBD
+
+; post-channel
+; check-collaborators
+; post-block-to-channel
+
+; Channel Put tests
+
+; update-channel-attributes
+; update-order
+; toggle-block
+
+; Search get tests
+
+(deftest search-test
+  (testing "Ability to search are.na for terms."
+    (is (= (:status (search-get/search "history")) 200))))
+
+(deftest search-users-test
+  (testing "Ability to search are.na for users."
+    (is (= (:status (search-get/search-users "history")) 200))))
+
+
+(deftest search-channels-test
+  (testing "Ability to search are.na for users."
+    (is (= (:status (search-get/search-channels "history")) 200))))
+
+
+(deftest search-blocks-test
+  (testing "Ability to search are.na for users."
+    (is (= (:status (search-get/search-blocks "history")) 200))))
+
+
+(deftest search-get-tests
+  (search-test)
+  (search-users-test)
+  (search-channels-test)
+  (search-blocks-test))
+
+
+; User get tests
+(deftest get-user-test
+  (testing "Ability to retrieve a specific user"
+    (is (= (:status (user-get/get-user "17")) 200))))
+
+(deftest get-user-channel-test
+  (testing "Ability to retrieve a specific user"
+    (is (= (:status (user-get/get-user-channel "17")) 200))))
+
+(deftest get-user-channels-test
+  (testing "Ability to retrieve a specific user"
+    (is (= (:status (user-get/get-user-channels "17")) 200))))
+
+(deftest get-follows-test 
+  (testing "Ability to retrieve channels/users an account is following"
+    (is (= (:status (user-get/get-follows "17"))))))
+
+
+(deftest get-followers-test
+  (testing "Ability to retrieve channels/users an account is following"
+    (is (= (:status (user-get/get-followers "17"))))))
+
+(deftest user-get-tests
+  (get-user-test)
+  (get-user-channel-test)
+  (get-user-channels-test)
+  (get-follows-test)
+  (get-followers-test))
