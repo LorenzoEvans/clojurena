590c0464928b3cce1ddf1624b68fa74efdbace3a
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-02-17 17:05:23 -0500

    working with these tests for repl use

--- test/core_test.clj
+++ test/core_test.clj
@@ -1,8 +1,18 @@
 (ns clojurena.core-test
   (:require [clojure.test :refer :all]
-            [clojurena.core :refer :all]
-            [clojurena.API.Channels :refer :all]          
-            [clojurena.API.Blocks :refer :all]))
+            [block-delete-tests :as block-del]
+            [block-get-tests :as block-get]
+            [block-post-tests :as block-post]
+            [block-put-tests :as block-put]
+            [channel-delete-tests :as channel-del]
+            [channel-get-tests :as channel-get]
+            [channel-post-tests :as channel-post]
+            [channel-put-tests :as channel-puts]
+            [search-get-tests :as search-get]
+            [user-get-tests :as user-get]))
+            
+            
+            
             
 
 (deftest a-test
